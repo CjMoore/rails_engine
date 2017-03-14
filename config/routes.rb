e20d1c9b75a5a11e1cd031625d1539083cdd8493
Charlotte Moore <mooc123@gmail.com>
2017-03-14 09:57:03 -0600

    adds random functionality to invoices

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'invoices_random/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       resources :invoices, only: [:index, :show] do
         collection do
           get "/find", to: "invoices_find#show"
           get "/find_all", to: "invoices_find#index"
+          get "/random", to: "invoices_random#show"
         end
       end
     end
