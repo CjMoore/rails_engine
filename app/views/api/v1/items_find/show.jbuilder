a2f33754166a7fa5db4b56ff2817729d28a6eed9
Charlotte Moore <mooc123@gmail.com>
2017-03-14 13:28:14 -0600

    adds jbuilder and strong params find_by and where

---
+++ app/views/api/v1/items_find/show.jbuilder
@@ -0,0 +1,2 @@
+json.(@item, :id, :name, :description, :merchant_id)
+json.unit_price (@item.unit_price/100.00).round(2)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  # namespace :api do
-  #   namespace :v1 do
-  #     get 'invoices_random/show'
-  #   end
-  # end
-
   namespace :api do
     namespace :v1 do
       resources :invoices, only: [:index, :show] do
