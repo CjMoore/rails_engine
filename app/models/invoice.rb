f1eb2cd4ca49570d2e3923b615f4145c0afa28ee
Charlotte Moore <mooc123@gmail.com>
2017-03-14 09:16:50 -0600

    extracts find and find all to application record

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,24 +5,4 @@ class Invoice < ApplicationRecord
   has_many :transactions
 
   validates :status, presence: true
-
-  def self.get_by_key(key, params)
-    if key == "created_at" || key == "updated_at"
-      date = params[key].to_datetime
-      find_by(key => date)
-    elsif key
-      find_by(key => params[key])
-    end
-  end
-
-  def self.get_all_by_key(key, params)
-    if key == "created_at" || key == "updated_at"
-      date = params[key].to_datetime
-      where(key => date)
-    elsif key
-      where(key => params[key])
-    end
-  end
-
-
 end
