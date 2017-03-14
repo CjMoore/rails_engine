9f78169508e42ff477461879a029263ee9b8385a
CJ <mooc123@gmail.com>
2017-03-14 14:33:25 -0600

    Update items_find_controller.rb
    removes get_key method from items find

--- app/controllers/api/v1/items_find_controller.rb
+++ app/controllers/api/v1/items_find_controller.rb
@@ -12,11 +12,4 @@ class Api::V1::ItemsFindController< ApplicationController
   def find_params
     params.permit(:name, :merchant_id, :id, :description, :unit_price, :created_at, :updated_at)
   end
-
-  def get_key
-    params.keys.find do |key|
-      [:name, :merchant_id, :id, :description, :unit_price, :created_at, :updated_at].include?(key.downcase.to_sym)
-    end
-  end
-
 end
