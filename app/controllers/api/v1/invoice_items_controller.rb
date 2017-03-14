dd45ab66c42b36fab0a2d548f702e68bc1d2ce71
Charlotte Moore <mooc123@gmail.com>
2017-03-14 11:28:35 -0600

    fixes spec harness tests

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::InvoiceItemsController < ApplicationController
 
   def index
-    render json: InvoiceItem.all.to_json(only: [:id, :customer_id, :merchant_id, :status])
+    render json: InvoiceItem.all.to_json(only: [:id, :quantity, :item_id, :invoice_id, :unit_price])
   end
 
   def show
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.all
+    render json: Item.all.to_json(only: [:id, :merchant_id, :name, :description, :unit_price])
   end
 
   def show
