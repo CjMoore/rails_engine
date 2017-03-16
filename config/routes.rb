e1e184efedb5d7ffc209ffb6280d7899a26e4ccc
Charlotte Moore <mooc123@gmail.com>
2017-03-15 19:23:14 -0600

    adds merchant invoices relationship endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 
       resources :merchants, only: [:index, :show] do
         get "/items", to: "merchants_items#index"
+        get "/invoices", to: "merchants_invoices#index"
         get "/favorite_customer", to: "merchants_favorite_customer#show"
         get "/revenue", to: "merchants_revenue#show"
         collection do
--- spec/requests/api/v1/merchants/merchant_relationships_spec.rb
+++ spec/requests/api/v1/merchants/merchant_relationships_spec.rb
@@ -16,4 +16,20 @@ describe "when user requests merchant relationship routes" do
       expect(item_response.last["id"]).to eq(item2.id)
     end
   end
+
+  context "for invoices" do
+    it "api responds with all invoices associated with that merchant" do
+      merchant = Fabricate(:merchant)
+      invoice1, invoice2 = Fabricate.times(2, :invoice, merchant: merchant)
+
+      get "/api/v1/merchants/#{merchant.id}/invoices"
+
+      expect(response).to be_success
+
+      invoice_response = JSON.parse(response.body)
+
+      expect(invoice_response.first["id"]).to eq(invoice1.id)
+      expect(invoice_response.last["id"]).to eq(invoice2.id)
+    end
+  end
 end
